495fc1d6990b4fb3dfa59729e255033a562473dd
hari-testpress <hari@testpress.in>
2023-12-15 23:03:47 +0530

    Support listing Complaints in admin portal

--- complaints/admin.py
+++ complaints/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Complaint
 
-# Register your models here.
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'description', 'staff', 'severity_level', 'timestamp')
+    list_filter = ('severity_level', 'timestamp')
+    search_fields = ('student_id', 'staff__username', 'description')
