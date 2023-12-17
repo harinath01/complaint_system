b2bbe40b0bb5a82adaa2cd012323ee776d882076
hari-testpress <hari@testpress.in>
2023-12-17 11:00:23 +0530

    Change complaint types text

--- complaints/models.py
+++ complaints/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 class Complaint(models.Model):
     COMPLAINT_TYPES = [
-        ('dresscode', 'Improper dresscode'),
+        ('dresscode', 'Improper dress code'),
         ('informal_shoe', 'Informal shoe'),
         ('hairstyle', 'Improper hairstyle'),
         ('untrimmed_beard', 'Untrimmed beard'),
-        ('socks_unworn', 'Socks unworn'),
-        ('shoe_unworn', 'Shoe unworn'),
-        ('blazer_unworn', 'Blazer unworn'),
+        ('socks_not_worn', 'Socks not worn'),
+        ('shoes_not_worn', 'Shoes not worn'),
+        ('blazer_not_worn', 'Blazer not worn'),
     ]
 
     student_id = models.CharField(max_length=16)
