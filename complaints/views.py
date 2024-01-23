e39c3e2e6ca64b1462bd02c85678a851e4b4296b
hari-testpress <hari@testpress.in>
2024-01-23 20:06:05 +0530

    Prevent raising complaint for same student multiple time on a day

--- complaints/views.py
+++ complaints/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
+from django.utils import timezone
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
@@ -16,13 +17,24 @@ def create_complaint(request):
     if request.method == 'POST':
         form = ComplaintForm(request.POST)
         if form.is_valid():
-            if Complaint.objects.filter(student_id=form.data["student_id"]).count() >= 3:
-                messages.error(request, 'Student already have three complaints')
-                return redirect('create_complaint')
-            form.save(staff=request.user)
-            messages.success(request, 'Complaint submitted successfully!')
+            student_id = form.cleaned_data["student_id"]
+            if has_duplicate_complaint(request.user, student_id):
+                messages.error(request,
+                               'You have already submitted a complaint for this student today.')
+            else:
+                form.save(staff=request.user)
+                complaints_count = Complaint.objects.filter(staff=request.user, student_id=student_id).count()
+                if complaints_count >= 3:
+                    messages.error(request, f'Complaint submitted successfully! Student reached {complaints_count} complaints')
+                else:
+                    messages.success(request, 'Complaint submitted successfully!')
             return redirect('create_complaint')
     else:
         form = ComplaintForm()
 
     return render(request, 'create_complaint.html', {'form': form})
+
+
+def has_duplicate_complaint(user, student_id):
+    today = timezone.now().date()
+    return Complaint.objects.filter(staff=user, student_id=student_id, timestamp__date=today).exists()
